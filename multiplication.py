981249dae1e14074789b3d3f1b605398383b889f
christinmendez <169026722+christinmendez@users.noreply.github.com>
2024-06-07 15:44:58 +0530

    Add files via upload

---
+++ multiplication.py
@@ -0,0 +1,7 @@
+for i in range(1,11):
+            print("table of",i)
+            for j in range(1,11):
+                print(j,"*",i,"*",i*j)      
+ 
+    
+   
---
+++ vowels.py
@@ -0,0 +1,9 @@
+def vowels(x):
+    vo=["a"or"e"or"i"or"o"or"u"or"A"or"E"or"I"or"O"or"u"]
+    count=0
+    for i in x:
+          if vo in vowels:
+                count=+1
+                print(count)
+    return  count
+x=input("enter a string: ")
